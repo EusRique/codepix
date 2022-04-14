b64a15943ce53a69039ccf0503d9d7ff8ebd149b
Henrique Camargo <h.camargo91@gmail.com>
2022-04-14 18:23:43 -0300

    feat: created method of validation

--- codepix/domain/model/transaction.go
+++ codepix/domain/model/transaction.go
@@ -1,5 +1,7 @@
 package model
 
+import "github.com/asaskevich/govalidator"
+
 type Transaction struct {
 	Base              `valid:"required"`
 	AccountFrom       *Account `valid:"-"`
@@ -8,4 +10,13 @@ type Transaction struct {
 	Status            string   `json:"status" valid:"notnull"`
 	Description       string   `json:"description" valid:"notnull"`
 	CancelDescription string   `json:"cancel_description" valid:"notnull"`
+}
+
+func (t *Transaction) isValid() error {
+	_, err := govalidator.ValidateStruct(t)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
