3f1e4aba8316a777fd1c52abdfe7ba6f7ae457d0
Henrique Camargo <h.camargo91@gmail.com>
2022-05-06 22:39:06 -0300

    feat: created method that implements FindBank

--- codepix/infrastructure/repository/pix.go
+++ codepix/infrastructure/repository/pix.go
@@ -61,3 +61,15 @@ func (r PixKeyRepositoryDb) FindAccount(id string) (*model.Account, error) {
 
 	return &account, nil
 }
+
+func (r PixKeyRepositoryDb) FindBank(id string) (*model.Bank, error) {
+	var bank model.Bank
+
+	r.DB.First(&bank, "id = ?", id)
+
+	if bank.ID == "" {
+		return nil, fmt.Errorf(" No bank found")
+	}
+
+	return &bank, nil
+}
