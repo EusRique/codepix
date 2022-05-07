ed9e6fbee4ed5e3d61aa466bf9c125db25c11de7
Henrique Camargo <h.camargo91@gmail.com>
2022-05-06 22:14:07 -0300

    feat: created method that implements add bank

--- codepix/infrastructure/repository/pix.go
+++ codepix/infrastructure/repository/pix.go
@@ -1,7 +1,19 @@
 package repository
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/EusRique/codepix/domain/model"
+	"github.com/jinzhu/gorm"
+)
 
 type PixKeyRepositoryDb struct {
 	DB *gorm.DB
+}
+
+func (r PixKeyRepositoryDb) AddBank(bank *model.Bank) error {
+	err := r.DB.Create(bank).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
