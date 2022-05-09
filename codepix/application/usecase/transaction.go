172c60d0a89581b72782d5201b4be36f5339dce0
Henrique Camargo <h.camargo91@gmail.com>
2022-05-09 18:50:47 -0300

    feat: create new register transaction

--- codepix/application/usecase/transaction.go
+++ codepix/application/usecase/transaction.go
@@ -1,8 +1,36 @@
 package usecase
 
-import "github.com/EusRique/codepix/domain/model"
+import (
+	"errors"
+
+	"github.com/EusRique/codepix/domain/model"
+)
 
 type TransactionUseCase struct {
 	TransactionRepository model.TransactionRepositoryInterface
 	PixRepository         model.PixKeyRepositoryInterface
+}
+
+func (t *TransactionUseCase) Register(accountId string, amount float64, pixKeyTo string, pixKeyKindTo string, description string) (*model.Transaction, error) {
+	account, err := t.PixRepository.FindAccount(accountId)
+	if err != nil {
+		return nil, err
+	}
+
+	pixKey, err := t.PixRepository.FindKeyByKind(pixKeyTo, pixKeyKindTo)
+	if err != nil {
+		return nil, err
+	}
+
+	transaction, err := model.NewTransaction(account, amount, pixKey, description)
+	if err != nil {
+		return nil, err
+	}
+
+	t.TransactionRepository.Save(transaction)
+	if transaction.ID != "" {
+		return transaction, nil
+	}
+
+	return nil, errors.New("Unable to process this transaction")
 }
