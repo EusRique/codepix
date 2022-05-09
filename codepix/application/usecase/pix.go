3b90cc2ba07ba159f815fa01fcced6620fad1c76
Henrique Camargo <h.camargo91@gmail.com>
2022-05-09 17:58:51 -0300

    create new register key

--- codepix/application/usecase/pix.go
+++ codepix/application/usecase/pix.go
@@ -1,1 +1,30 @@
 package usecase
+
+import (
+	"errors"
+
+	"github.com/EusRique/codepix/domain/model"
+)
+
+type PixUseCase struct {
+	PixKeyRepository model.PixKeyRepositoryInterface
+}
+
+func (p *PixUseCase) RegisterKey(key string, kind string, accountId string) (*model.PixKey, error) {
+	account, err := p.PixKeyRepository.FindAccount(accountId)
+	if err != nil {
+		return nil, err
+	}
+
+	pixKey, err := model.NewPixKey(kind, account, key)
+	if err != nil {
+		return nil, err
+	}
+
+	p.PixKeyRepository.RegisterKey(pixKey)
+	if pixKey.ID == "" {
+		return nil, errors.New("Unable to create new key at the moment")
+	}
+
+	return pixKey, nil
+}
