2f1f3e5fb5b459bd9f4ae48caf4ac107d26702d4
Henrique Camargo <h.camargo91@gmail.com>
2022-05-09 15:44:09 -0300

    feat: created method that implements find

--- codepix/infrastructure/repository/transaction.go
+++ codepix/infrastructure/repository/transaction.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"fmt"
+
 	"github.com/EusRique/codepix/domain/model"
 	"gorm.io/gorm"
 )
@@ -21,6 +23,19 @@ func (t *TransactionRepositoryDb) Register(transaction *model.Transaction) error
 func (t *TransactionRepositoryDb) Save(transaction *model.Transaction) error {
 	err := t.Db.Save(transaction).Error
 	if err != nil {
-		return nil
+		return err
 	}
+
+	return nil
+}
+
+func (t *TransactionRepositoryDb) find(id string) (*model.Transaction, error) {
+	var transaction model.Transaction
+	t.Db.Preload("AccountFrom.Bank").First(&transaction, "id = ?", id)
+
+	if transaction.ID == "" {
+		return nil, fmt.Errorf("No transaction was found")
+	}
+
+	return &transaction, nil
 }
